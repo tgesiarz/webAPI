64cc09a9355506c45f60e3d2286ce855f962b1ae
tomcio <tgesiarz@wp.pl>
2017-11-17 15:14:40 +0100

    working get, post, put, delete

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// body parser middleware
 app.use(bodyParser.json());
+
+// router middleware
 app.use('/api', require('./routes/api'));
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    res.status(442).send({err: err.message});
+});
+
 app.listen(3000);
 console.log("listening on port...");
--- routes/api.js
+++ routes/api.js
@@ -3,26 +3,31 @@ const conn = require('./../data_access/db');
 const router = express.Router();
 
 // get list of cars
-router.get('/cars', (req, res) => {
+router.get('/cars', (req, res, next) => {
   conn.executeSQL("SELECT ID, Item FROM TODO", (err, rowCount, rows ) => {
     res.send(rows);
   });
 });
 
 // add a new car to the database
-router.post('/cars', (req, res) => {
-  console.log(req.body);
-  res.send({type: 'POST'});
+router.post('/cars', (req, res, next) => {
+  conn.executeSQL("INSERT INTO TODO VALUES('" + req.body.item + "')", (err, rowCount, rows) => {
+    res.send("success...");
+  });
 });
 
 // update existing car in the database
-router.put('/cars/:id', (req, res) => {
-  res.send({id: req.params.id});
+router.put('/cars/:id', (req, res, next) => {
+  conn.executeSQL("UPDATE TODO SET ITEM = '" + req.body.item + "' WHERE ID = " + req.params.id, (err, rowCount, rows) => {
+    res.send("success...");
+  });
 });
 
 // delete car from the database
-router.delete('/cars/:id', (req, res) => {
-  res.send({type: 'DELETE'});
+router.delete('/cars/:id', (req, res, next) => {
+  conn.executeSQL("DELETE TODO WHERE ID = " + req.params.id, (err, rowCount, rows) => {
+    if(!err) { res.send("success...") } else return next(err);
+  });
 });
 
 module.exports = router;
